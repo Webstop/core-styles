17568dcfa8e2bc9d9d7b0be0e05c0d9f6e66e885
Dan Lynn <git@danlynn.org>
2016-11-30 15:32:00 -0500

    Dockerized project
    Run all jekyll commands like `./jekyll serve` now.

--- build_docker.sh
+++ build_docker.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+# Make sure that you are logged in first:
+#
+#   $ docker login
+#   Username: danlynn
+#   Password: ******
+#   Login Succeeded
+
+docker build -t webstop/jekyll:latest .
+
+docker push webstop/jekyll
