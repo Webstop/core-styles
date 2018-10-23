d85f246b5c5de67a46745f829ffc09a591a9203f
Greg Hemphill <greg@webstop.com>
2018-10-23 15:59:23 -0400

    adds standard coupon

--- dist/js/core-styles/site-modal.js
+++ dist/js/core-styles/site-modal.js
@@ -2,9 +2,6 @@
 // Site Modal Component
 
 $(function() {
-
-
-
   // Resets modal once closed
   $('#site-modal').on('hidden.bs.modal', function () {
     $('#site-modal-title').text('');
@@ -12,20 +9,20 @@ $(function() {
     $('#site-modal-footer').removeClass('d-none');
   });
 
-});
-
-$('[data-site-modal="true"]').on('click', function(event){
-  event.preventDefault();
-  let $this = $(this);
-  let title = $this.attr('data-title');
-  let url = $this.attr('data-load');
-  let footer = $this.attr('data-footer');
-
-  if(footer == 'false'){
-    $('#site-modal-footer').addClass('d-none');
-  }
-  $('#site-modal-title').text(title);
-  $('#site-modal-body').load(url);
+  $('[data-site-modal="true"]').on('click', function(event){
+    event.preventDefault();
+    let $this = $(this);
+    let title = $this.attr('data-title');
+    let url = $this.attr('data-load');
+    let footer = $this.attr('data-footer');
+    if(footer == 'false'){
+      $('#site-modal-footer').addClass('d-none');
+    }
+    $('#site-modal-title').text(title);
+    $('#site-modal-body').load(url);
+  });
 
 });
+
+
 
