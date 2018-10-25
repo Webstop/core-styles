9ee6c4d958458f0af822d69efc17136bae286780
Greg Hemphill <greg@webstop.com>
2018-10-25 00:41:11 -0400

    sticky toolbars & sticky power bar

--- dist/js/core-styles/shopping-list-power-bar.js
+++ dist/js/core-styles/shopping-list-power-bar.js
@@ -1,22 +1,17 @@
 "use strict";
-// Site Modal Component
 
 // TODO: For now this is global. It really should be loaded as an es6 module instead.
-function loadShoppingListPowerBar(added = false){
-  $('#shopping-list-power-bar').load('/shopping_list/power_bar', function (added) {
-    if(added) {
-      $('.shopping-list-power-bar-item:first-child').addClass('alert-warning').delay( 800 ).removeClass('alert-warning');
-    }
-  });
+function loadShoppingListPowerBar(){
+  $('#site-aside-slider').load('/shopping_list/power_bar');
 }
 
 $(function() {
 
   loadShoppingListPowerBar();
 
-  $('.shopping-list-power-bar-toggle').on('click', function(event){
+  $('.site-aside-slider-toggle').on('click', function(event){
     event.preventDefault();
-    $('#shopping-list-power-bar').toggleClass('shopping-list-power-bar-open');
+    $('#site-aside-slider').toggleClass('site-aside-slider-open');
   });
 
 });
