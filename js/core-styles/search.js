330406205c17c89739d697f679894ca0c6ab0e2a
Greg Hemphill <greg@webstop.com>
2018-11-21 17:23:44 -0500

    Completes Live Search functionality.

--- js/core-styles/search.js
+++ js/core-styles/search.js
@@ -2,57 +2,31 @@
 // Search Component
 
 $(function() {
-  function liveSearch(){
 
+  function liveSearch(){
     let delay = null;
 
-
-
-
     $('[data-search="live"]').find('.search-from-search-text').on('keyup', function(event) {
-
       clearTimeout(delay);
-
       let $form = $(this).parents('[data-search="live"]');
       let target = $form.data('target');
-      let searchText = event.target.value;
+      let search = event.target.value;
       let url = $form.attr('action');
+      //console.log('typed: ' + search);
 
-      console.log('typed: ' + searchText);
-
-      if(searchText.lenght <= 2){
-        // clear target
-        $target.html(' ');
+      if(search.length <= 2){
+        $(target).html('');
       } else {
-        //delay = setTimeout(function(){ performSearch(searchText, target, url) }, 500);
-        performSearch(searchText, target, url)
+        delay = setTimeout(function(){ performSearch(search, target, url) }, 500);
       }
-
-      //setTimeout(function(){ alert("Hello"); }, 3000);
-
     });
-
   }
 
-  function performSearch(searchText, target, url){
-    console.log('search: ' + searchText);
-    console.log(target);
-    console.log($(target));
-    console.log('url:' + url);
-    // load here
-    $(target).load(url, { search: searchText });
+  function performSearch(search, target, url){
+    //console.log('search: ' + search);
+    $(target).load(url, { search: search });
   }
 
   liveSearch();
 });
 
-// var timeoutID = null;
-//
-// function findMember(str) {
-//   console.log('search: ' + str);
-// }
-//
-// $('#target').keyup(function(e) {
-//   clearTimeout(timeoutID);
-//   timeoutID = setTimeout(findMember.bind(undefined, e.target.value), 500);
-// });
