461d7080485d82e98cb9a127a39c7b26077f8457
Greg Hemphill <greg@webstop.com>
2018-12-12 16:39:23 -0500

    progress on shopping list modals

--- js/core-styles/shopping-list-power-bar.js
+++ js/core-styles/shopping-list-power-bar.js
@@ -14,6 +14,76 @@ $(function() {
     $('#site-aside-slider').toggleClass('site-aside-slider-open');
   });
 
+
+  let $modal = $('#shopping-list-modal')
+
+  $modal.on('show.bs.modal', function(event) {
+    let $form = $modal.find('.shopping-list-form')
+    let $errors = $form.find('.shopping-list-form-errors')
+    let button = $(event.relatedTarget) // Button that triggered the modal
+    //  verbs: Create (new), Update (rename/edit), Delete (delete)
+    let verb = button.data('verb') // Extract info from data-* attributes
+    let response = {status: '', message: ''}
+    let $name = $modal.find('.modal-body .shopping-list-form-name')
+
+    $modal.find('.modal-title').text(verb + ' Shopping List')
+    $errors.html('')
+
+    if(verb == 'Delete'){
+      $modal.find('.shopping-list-form-name-group').addClass('is-hidden')
+    } else {
+      $modal.find('.shopping-list-form-name-group').removeClass('is-hidden')
+    }
+
+    if(verb == 'Create'){
+      let today = new Date();
+      today = today.toLocaleDateString('en-US', {year: 'numeric', month: 'numeric', day: 'numeric' })
+      $name.val('My ' + today + ' List')
+    }
+
+    let url = '/shopping_lists/List/' + verb;
+
+    $form.attr('action', url);
+
+    // Form response expects the following JSON:
+    // {
+    //   status: 'success' or 'failure',
+    //   message: 'text message here'
+    // }
+    $form.on('submit', function(e) {
+      e.preventDefault();
+      // Do ajax calling url
+      let request = $.ajax({
+        url: url,
+        method: 'POST',
+        //data: { id : menuId },
+        dataType: 'json'
+      });
+
+      request.done(function(data){
+        if(data.status == 'success'){
+          response = data
+          $modal.hide()
+        } else if(data.status == 'failure') {
+          response = data
+        } else {
+          response = {status: 'failure', message: 'Unable to process request. Please try again.'}
+        }
+
+        if(response.status == 'failure') {
+          $errors.html('<p class="shopping-list-form-errors-message alert alert-danger">' + response.message + '</p>');
+        }
+
+      })
+    });
+
+  })
+
+  $modal.on('hidden.bs.modal', function() {
+    loadShoppingListPowerBar();
+  })
+
+
 });
 
 
