022b69ea1b52f085393690d57a312e00ad84816c
Greg Hemphill <greg@webstop.com>
2018-12-14 09:23:40 -0500

    adds ability to include content from the DOM (untested)

--- js/core-styles/site-modal.js
+++ js/core-styles/site-modal.js
@@ -5,7 +5,7 @@ $(function() {
   // Resets modal once closed
   $('#site-modal').on('hidden.bs.modal', function () {
     $('#site-modal-title').text('');
-    $('#site-modal-body').text('');
+    $('#site-modal-body').html('');
     $('#site-modal-footer').removeClass('d-none');
   });
 
@@ -14,12 +14,21 @@ $(function() {
     let $this = $(this);
     let title = $this.attr('data-title');
     let url = $this.attr('data-load');
+    let content = $this.attr('data-content');
     let footer = $this.attr('data-footer');
     if(footer == 'false'){
       $('#site-modal-footer').addClass('d-none');
     }
     $('#site-modal-title').text(title);
-    $('#site-modal-body').load(url);
+
+    if(content){
+      $('#site-modal-body').html(content);
+    }
+
+    if(url){
+      $('#site-modal-body').load(url);
+    }
+
   });
 
 });
